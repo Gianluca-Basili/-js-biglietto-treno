133b5c23d19102cf406c31f79647e97e1c9fabb9
Gianluca-Basili <128229910+Gianluca-Basili@users.noreply.github.com>
2023-05-17 15:58:29 +0200

    add prompt e calcoli biglietto

--- main.js
+++ main.js
@@ -12,19 +12,19 @@ console.log( prezzo_base);
 
 // calcolo biglietto eta inferiroe ai 18 anni
 if(eta<18){
-    console.log(eta)
-    prezzo_biglietto = (prezzo_base * 20/100)
-    console.log(prezzo_biglietto)
+    console.log(eta);
+    prezzo_biglietto = (prezzo_base * 20/100);
+    console.log(prezzo_biglietto);
 
 }else if (eta >= 18 && eta <= 65) //calcolo biglietto eta compresa tra i 18 e i 65
 {
-    console.log(eta)
-    prezzo_biglietto = (prezzo_base)
-    console.log(prezzo_biglietto)
+    console.log(eta);
+    prezzo_biglietto = (prezzo_base);
+    console.log(prezzo_biglietto);
 
 }else if (eta > 65) //calcolo biglietto eta maggiore di 65 anni
 {
-    prezzo_biglietto = (prezzo_base * 40/100)
-    console.log(prezzo_biglietto)
+    prezzo_biglietto = (prezzo_base * 40/100);
+    console.log(prezzo_biglietto);
 }
 
