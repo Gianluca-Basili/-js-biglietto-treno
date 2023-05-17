4e7a7c70ddad58a8f7a3faf493851c2f4498e399
Gianluca-Basili <128229910+Gianluca-Basili@users.noreply.github.com>
2023-05-17 17:08:35 +0200

    esercizio finito

--- main.js
+++ main.js
@@ -10,20 +10,28 @@ console.log(eta);
 let prezzo_base = (chilometri * 0.21);
 console.log( prezzo_base);
 
+let prezzo_biglietto
+
+
 // calcolo biglietto eta inferiore ai 18 anni
 if(eta<18){
     console.log(eta);
     prezzo_biglietto = (prezzo_base * 20/100);
     console.log(prezzo_biglietto);
+    
 
-}else if (eta >= 18 && eta <= 65) //calcolo biglietto eta compresa tra i 18 e i 65
+}else if  (eta > 65) //calcolo biglietto eta maggiore di 65 anni
 {
+    prezzo_biglietto = (prezzo_base * 40/100);
+    console.log(prezzo_biglietto);
+}else {
     console.log(eta)
     prezzo_biglietto = (prezzo_base)
     console.log(prezzo_biglietto)
 
-}else if (eta > 65) //calcolo biglietto eta maggiore di 65 anni
-{
-    prezzo_biglietto = (prezzo_base * 40/100);
-    console.log(prezzo_biglietto);
 }
+
+// aggiunta dei due valori decimali
+const decimale = prezzo_biglietto.toFixed(2)
+console.log(decimale)
+console.log(typeof decimale)
